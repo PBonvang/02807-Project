fe3d896a8a21f0f811c42f55b4a327710c567dc4
PB <Peter.bonvang@gmail.com>
2023-11-14 23:27:44 +0100

    DAR join created

--- Utils/preprocessing.py
+++ Utils/preprocessing.py
@@ -25,7 +25,7 @@ def get_file_metadata(data_path: Path, save_path: Path = None)->dict:
 
     with tqdm(total=data_path.stat().st_size) as pbar:
         with data_path.open(encoding='utf-8') as file:
-            list_attributes = []
+            sample = {}
             determine_attributes = False
 
             i = 0
@@ -34,24 +34,30 @@ def get_file_metadata(data_path: Path, save_path: Path = None)->dict:
                 if LIST_IDENTIFIER in line:
                     if list_name in file_metadata: 
                         file_metadata[list_name]['end_line'] = i-2
+                        file_metadata[list_name]['attributes'] = list(sample.keys())
+                        file_metadata[list_name]['sample'] = sample
 
                     list_name = get_attribute_from_json_line(line)
-                    file_metadata[list_name] = {'start_line':i, 'attributes':list_attributes}
+                    file_metadata[list_name] = {'start_line':i}
 
+                    sample = {}
                     determine_attributes = True
                 
                 elif determine_attributes and '}' in line:
                     determine_attributes = False
                 
                 elif determine_attributes and ':' in line:
-                    list_attributes.append(get_attribute_from_json_line(line))
+                    attribute, value = get_json_key_value_pair(line)
+                    sample[attribute] = value
                     
                 i += 1
                 if not i % 1000:
-                    pbar.update(1000)
+                    pbar.update(file.tell() - pbar.n)
 
             if list_name in file_metadata: 
                 file_metadata[list_name]['end_line'] = i-2
+                file_metadata[list_name]['attributes'] = list(sample.keys())
+                file_metadata[list_name]['sample'] = sample
                 
     if save_path is not None:
         with save_path.open('w', encoding='utf-8') as outfile:
@@ -64,18 +70,23 @@ def get_json_key_value_pair(json_line: str) -> tuple[str]:
         map(str.strip, json_line\
             .replace('"','')\
             .replace(',\n','')\
+            .replace('\n','')\
             .split(':',1))
     )
     return attribute, value
 
 def json_list_to_csv(json_file: Path,
                      list_metadata: dict,
-                     output_file: Path) -> None:
+                     output_file: Path,
+                     attributes: list[str] = None) -> None:
     
+    if attributes is None:
+        attributes = list_metadata['attributes']
+
     with tqdm(total=list_metadata['end_line']) as pbar:
         with json_file.open(encoding='utf-8') as file:
             with output_file.open('w', encoding='utf-8',newline='') as output_file:
-                csvwriter = csv.DictWriter(output_file, fieldnames=list_metadata['attributes'])
+                csvwriter = csv.DictWriter(output_file, fieldnames=attributes, delimiter=";")
                 csvwriter.writeheader()
 
                 data_point = {}
@@ -90,7 +101,8 @@ def json_list_to_csv(json_file: Path,
                     
                     if ':' in line:
                         attribute, value = get_json_key_value_pair(line)
-                        data_point[attribute] = value
+                        if attribute in attributes:
+                            data_point[attribute] = value
 
                     if '}' in line and len(data_point.keys()) > 0:
                         csvwriter.writerow(data_point)
--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@
 %load_ext autoreload
 %autoreload 2
 # %% Imports
+import json
 import pandas as pd
 from pathlib import Path
 from data.DAR import DAR_PATH
@@ -11,15 +12,87 @@ from utils.preprocessing import get_file_metadata, json_list_to_csv
 #############################################################
 # DAR
 #############################################################
-# %% Metadata
+# %% Generate Metadata
 DAR_path = Path('D:/DAR_Aktuelt_Totaludtraek_JSON_HF_20231105180006/DAR_Aktuelt_Totaludtraek_JSON_HF_20231105180006.json')
 
 DAR_metadata = get_file_metadata(DAR_path, Path('data/DAR_metadata.json'))
 DAR_metadata
-# %% AdresseList to csv
+# %% Load Metadata
+DAR_metadata = json.load
+########################## DAR data to csv ##########################
+# %% HusnummerList
+HusnummerList_path = Path('data/DAR/DAR_HusnummerList.csv')
 json_list_to_csv(
     DAR_PATH,
-    DAR_metadata['AdresseList'],
-    Path('data/DAR_adresse_list.csv'))
+    DAR_metadata['HusnummerList'],
+    HusnummerList_path,
+    attributes=[
+        'id_lokalId',
+        'status',
+        'adgangsadressebetegnelse',
+        'husnummertekst',
+        'navngivenVej',
+        'postnummer'
+    ])
 
+# %% PostnummerList
+PostnummerList_path = Path('data/DAR/DAR_PostnummerList.csv')
+json_list_to_csv(
+    DAR_PATH,
+    DAR_metadata['PostnummerList'],
+    PostnummerList_path,
+    attributes=[
+        'id_lokalId',
+        'status',
+        'navn',
+        'postnr'
+    ])
+# %% NavngivenVejList
+NavngivenVejList_path = Path('data/DAR/DAR_NavngivenVejList.csv')
+json_list_to_csv(
+    DAR_PATH,
+    DAR_metadata['NavngivenVejList'],
+    NavngivenVejList_path,
+    attributes=[
+        'id_lokalId',
+        'status',
+        'vejnavn'
+    ])
+
+########################## Join DAR files ##########################
+# %% Loading DAR data
+HusnummerList_data = pd.read_csv(HusnummerList_path, delimiter=';')\
+                        .rename(columns={
+                            'id_lokalId':'husnummer_id',
+                            'adgangsadressebetegnelse': 'address',
+                            'husnummertekst':'house_nr',
+                            'navngivenVej':'street_id',
+                            'postnummer':'postal_id'
+                        })
+NavngivenVejList_data = pd.read_csv(NavngivenVejList_path, delimiter=';')\
+                        .rename(columns={
+                            'id_lokalId':'street_id',
+                            'vejnavn':'street_name'
+                        })
+PostnummerList_data = pd.read_csv(PostnummerList_path, delimiter=';')\
+                        .rename(columns={
+                            'id_lokalId':'postal_id',
+                            'navn': 'city_name',
+                            'postnr':'postal_nr'
+                        })
+# %% Filter addresses
+has_street = ~pd.isna(HusnummerList_data['street_id'])
+is_house = HusnummerList_data['house_nr'].apply(lambda x: str.isnumeric(str(x)))
+
+DAR_data = HusnummerList_data.loc[has_street & is_house]
+
+DAR_data
+# %% Join DAR data
+DAR_joined_data = DAR_data.merge(NavngivenVejList_data.loc[:,['street_id','street_name']], how='left', on='street_id')\
+                    .merge(PostnummerList_data.loc[:, ['postal_id','city_name','postal_nr']], how='left', on='postal_id')\
+                    .loc[:, ['husnummer_id','address','house_nr','street_name','city_name','postal_nr']]
+DAR_joined_data
+# %% Save DAR data
+DAR_joined_data_path = Path('data/DAR/DAR_joined.csv')
+DAR_joined_data.to_csv(DAR_joined_data_path, sep=';', index=False)
 # %%
