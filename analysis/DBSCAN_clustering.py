de489f160f9b4b181626b7c29a8e6e4f9b99986e
PBonvang <peter.bonvang@gmail.com>
2023-11-25 18:02:25 +0100

    DBSCAN minmax normalized clustering

--- analysis/DBSCAN_clustering.py
+++ analysis/DBSCAN_clustering.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from sklearn.cluster import DBSCAN
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 # %% Load data
 print('[Loading data] Start')
 ds = pd.read_csv('data/DS_reduced_and_dropped.csv', index_col='id')
@@ -14,6 +15,9 @@ total_address_values = ds[prediction_property]
 # Feature data
 feature_names = ds.columns[ds.columns != prediction_property]
 feature_data = ds[feature_names]
+
+scaler = MinMaxScaler()
+normalized_feature_data = scaler.fit_transform(feature_data)
 # %% Hyperparameters
 # https://medium.com/@tarammullin/dbscan-parameter-estimation-ff8330e3a3bd
 eps = 329
@@ -22,8 +26,8 @@ min_samples = 2*len(feature_names)
 # %% Fit clusters
 print('[Determining clusters] Start')
 dbscan = DBSCAN(eps=eps, min_samples=min_samples, n_jobs=-1)
-dbscan.fit(feature_data)
+dbscan.fit(normalized_feature_data)
 print('[Determining clusters] Done')
 # %% Save clusters
 db_clusterings = dbscan.labels_
-np.save(f'data/DBSCAN_clusterings_eps{eps}_ms{min_samples}_DS_reduced_and_dropped.npy', db_clusterings)
+np.save(f'data/clusterings/DBSCAN_clusterings_minmax_eps{eps}_ms{min_samples}_DS_reduced_and_dropped.npy', db_clusterings)
